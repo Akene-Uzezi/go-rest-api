9582f1083d8b7a4972944c426b91e5f122269f34
Akene Uzezi <xtremejoexj@gmail.com>
2026-05-26 15:58:11 +0100

    wrote the middleware function to check if user is authenticated

---
+++ cmd/api/middleware.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+)
+
+func (app *application) AuthMiddleware() gin.HandlerFunc {
+	return func (c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header not provided"})
+			c.Abort()
+			return 
+		}
+
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+		if tokenStr == authHeader {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token. Bearer token required"})
+			c.Abort()
+			return 
+		}
+
+		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, jwt.ErrSignatureInvalid
+			}
+			return []byte(app.jwtSecret), nil
+		})
+
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		userId := claims["userId"].(float64)
+		user, err := app.models.Users.Get(int(userId), c.Request.Context())
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unathorized access"})
+			c.Abort()
+			return
+		}
+		c.Set("user", user)
+	}
+}
